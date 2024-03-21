78bc8c4c7425a3cd9ca6ee20540b9f4be04921fc
Renatoyyz <witcarerenato@gmail.com>
2024-03-21 16:13:53 -0300

    Acertando o modbus Wellpro

--- drive_central/Controller/ModbusRTU.py
+++ drive_central/Controller/ModbusRTU.py
@@ -14,6 +14,8 @@ class RTU485(threading.Thread):
         self._devices_address = devices_address
         self._mode = mode
 
+        self._out = [0,0,0,0]
+
         self.in_out = InOut()
 
     def crc16_modbus(self, data):
@@ -59,12 +61,137 @@ class RTU485(threading.Thread):
 
         # Repete-se os comandos em decimal com os devidos bytes de CRC
         self._serial.write([id_loc, 3,0,1,0,2,parte_inferior,parte_superior])
-        self._serial.flush()
+        # self._serial.flush()
         self.in_out.re_de_485(self.in_out.RECV_485)
 
         while self._serial.readable()==False:
             pass
         dados_recebidos = self._serial.read(7)
+        # self._serial.flush()
+        try:
+            # "01030207f0ba30"
+            # "020302020303bd23"
+            dados_recebidos = dados_recebidos.hex()
+            hex_text = dados_recebidos[0:2]+dados_recebidos[2:4]+dados_recebidos[4:6]+dados_recebidos[6:8]+dados_recebidos[8:10]
+            bytes_hex = bytes.fromhex(hex_text) # Transforma em hexa
+            crc_result = self.crc16_modbus(bytes_hex) # Retorna o CRC
+
+            parte_superior = (crc_result >> 8) & 0xFF  # Desloca 8 bits para a direita e aplica a máscara 0xFF
+            parte_inferior = crc_result & 0xFF        # Aplica a máscara 0xFF diretamente
+
+            superior_crc = int(dados_recebidos[12:14],16) # Transforma de hexa para int
+            inferior_crc = int(dados_recebidos[10:12],16) # Transforma de hexa para int
+
+            if parte_superior == superior_crc and parte_inferior == inferior_crc:
+                dados_recebidos = dados_recebidos[6:10]
+                dados_recebidos = int(dados_recebidos)
+                # time.sleep(0.5)
+                return dados_recebidos
+            else:
+                return -1
+        except:
+            return -1 # Indica erro de alguma natureza....
+        
+    def set_out(self, do_01=None, do_02=None, do_03=None, do_04=None):
+        if do_01 != None:
+            self._out[3]=do_01
+        if do_02 != None:
+            self._out[2]=do_02
+        if do_03 != None:
+            self._out[1]=do_03
+        if do_04 != None:
+            self._out[0]=do_04
+        return self.adam_wp9038()
+
+
+    def adam_wp9038(self):
+        
+        dados_recebidos = None
+        self.in_out.re_de_485(self.in_out.SEND_485)
+
+        id_loc = hex(self._devices_address['mod-adam'])[2:]
+        id_loc = id_loc.zfill(2).upper()
+
+        out_loc = ''.join(str(bit) for bit in self._out)
+        out_loc = int(out_loc, 2)
+        out_val = out_loc
+
+        out_loc = hex(out_loc)[2:]
+        out_loc = out_loc.zfill(2).upper()
+
+        hex_text = f"{id_loc} 0f 00 00 00 04 01 {out_loc}"
+        #01 0F 00 00 00 04 01 03
+        bytes_hex = bytes.fromhex(hex_text) # Transforma em hexa
+
+        crc_result = self.crc16_modbus(bytes_hex) # Retorna o CRC
+
+        parte_superior = (crc_result >> 8) & 0xFF  # Desloca 8 bits para a direita e aplica a máscara 0xFF
+        parte_inferior = crc_result & 0xFF        # Aplica a máscara 0xFF diretamente
+
+        id_loc = self._devices_address['mod-adam']
+
+        # Repete-se os comandos em decimal com os devidos bytes de CRC
+        # self._serial.flush()
+        self._serial.write([id_loc,0x0f,0,0,0,4,1,out_val,parte_inferior,parte_superior])
+        self._serial.flush()
+        self.in_out.re_de_485(self.in_out.RECV_485)
+        time.sleep(0.2)
+
+        while self._serial.readable()==False:
+            pass
+        dados_recebidos = self._serial.read(8)
+        try:
+            # "01030207f0ba30"
+            # "020302020303bd23"
+            #00 00 04 54 08
+            dados_recebidos = dados_recebidos.hex()
+            hex_text = dados_recebidos[0:2]+dados_recebidos[2:4]+dados_recebidos[4:6]+dados_recebidos[6:8]+dados_recebidos[8:10]+dados_recebidos[10:12]
+            bytes_hex = bytes.fromhex(hex_text) # Transforma em hexa
+            crc_result = self.crc16_modbus(bytes_hex) # Retorna o CRC
+
+            parte_superior = (crc_result >> 8) & 0xFF  # Desloca 8 bits para a direita e aplica a máscara 0xFF
+            parte_inferior = crc_result & 0xFF        # Aplica a máscara 0xFF diretamente
+
+            superior_crc = int(dados_recebidos[14:16],16) # Transforma de hexa para int
+            inferior_crc = int(dados_recebidos[12:14],16) # Transforma de hexa para int
+
+            if parte_superior == superior_crc and parte_inferior == inferior_crc:
+                dados_recebidos = dados_recebidos[14:16]
+                dados_recebidos = int(dados_recebidos)
+                # time.sleep(0.5)
+                return dados_recebidos
+            else:
+                return -1
+        except:
+            return -1 # Indica erro de alguma natureza....
+        
+    def broadcast(self):
+        
+        dados_recebidos = None
+        self.in_out.re_de_485(self.in_out.SEND_485)
+
+        id_loc = "00" # hex(self._devices_address['mod-eda'])[2:]
+        id_loc = id_loc.zfill(2).upper()
+
+        hex_text = f"{id_loc}0600640001"
+
+        bytes_hex = bytes.fromhex(hex_text) # Transforma em hexa
+
+        crc_result = self.crc16_modbus(bytes_hex) # Retorna o CRC
+
+        parte_superior = (crc_result >> 8) & 0xFF  # Desloca 8 bits para a direita e aplica a máscara 0xFF
+        parte_inferior = crc_result & 0xFF        # Aplica a máscara 0xFF diretamente
+
+        id_loc = 0 # self._devices_address['mod-adam']
+
+        # Repete-se os comandos em decimal com os devidos bytes de CRC
+        self._serial.write([id_loc, 6,0,0x64,0,1,parte_inferior,parte_superior])
+        self._serial.flush()
+        self.in_out.re_de_485(self.in_out.RECV_485)
+
+        while self._serial.readable()==False:
+            pass
+        dados_recebidos = self._serial.read(21)
         # self._serial.flush()
         try:
             # "01030207f0ba30"
--- drive_central/testes.py
+++ drive_central/testes.py
@@ -3,19 +3,21 @@ import serial
 from time import sleep
 
 ser = serial.Serial(
-    #port='/dev/ttyUSB0',  # Porta serial padrão no Raspberry Pi 4
-    port='/dev/ttyS0',  # Porta serial padrão no Raspberry Pi 4
+    port='/dev/ttyUSB0',  # Porta serial padrão no Raspberry Pi 4
+    # port='/dev/ttyS0',  # Porta serial padrão no Raspberry Pi 4
     baudrate=9600,       # Taxa de baud
     bytesize=8,
     parity="N",
     stopbits=1,
     timeout=1,            # Timeout de leitura
-    xonxoff=False,         # Controle de fluxo por software (XON/XOFF)
+    #xonxoff=False,         # Controle de fluxo por software (XON/XOFF)
     #rtscts=True
 )
+
 devices = {
         "mod-eda": 2,
-        "mod-pta9b": 1
+        "mod-pta9b": 1,
+        "mod-adam" : 1
 }
 
 rtu = RTU485(ser, devices_address=devices)
@@ -33,6 +35,22 @@ while(cmd.upper() != "Q"):
             retorno = rtu.temp_pta9b()
             print(f'Modulo ptb9: {retorno}\n')
         if cmd == "3":
+            cmd = input("Digite a saída\n1 = out_01\n2 = out_02\n3 = out_03\n4 = out_04\n")
+            if int(cmd) == 1:
+               cmd = input(f"Digite 1 para ligar e 0 para desligar a saída {int(cmd)}\n") 
+               retorno = rtu.set_out(do_01=int(cmd))
+            if int(cmd) == 2:
+               cmd = input(f"Digite 1 para ligar e 0 para desligar a saída {int(cmd)}\n") 
+               retorno = rtu.set_out(do_02=int(cmd))
+            if int(cmd) == 3:
+               cmd = input(f"Digite 1 para ligar e 0 para desligar a saída {int(cmd)}\n") 
+               retorno = rtu.set_out(do_03=int(cmd))
+            if int(cmd) == 4:
+               cmd = input(f"Digite 1 para ligar e 0 para desligar a saída {int(cmd)}\n") 
+               retorno = rtu.set_out(do_04=int(cmd))
+            
+            print(f'Modulo adam: {retorno}\n')
+        if cmd == "4":
             retorno = rtu.mod_eda() #rtu.enviar_e_receber_dados(int(cmd))
             print(f'Modulo ead: {retorno}\n')
             sleep(1)
